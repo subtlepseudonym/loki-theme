c8aaad4c6771b203a68b0b275faae8e81967a171
Connor Demille <subtlepseudonym@gmail.com>
2020-03-06 13:56:52 -0500

    Update install script to work with dotfiles

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cp `find "${dir}" -name "*\.zsh"` "${HOME}/.oh-my-zsh/custom/"
-cp `find "${dir}" -name "*\.zsh-theme"` "${HOME}/.oh-my-zsh/custom/themes"
+if [ -d "${HOME}/.oh-my-zsh" ]; then
+	cp `find "${dir}" -name "*\.zsh"` "${HOME}/.oh-my-zsh/custom/"
+	cp `find "${dir}" -name "*\.zsh-theme"` "${HOME}/.oh-my-zsh/custom/themes"
+else if [ ! -z "${DOTFILES}" ]; then
+	cp `find "${dir}" -name "*\.zsh"` "${ZSH}"
+	cp `find "${dir}" -name "*\.zsh-theme"` "${DOTFILES}/themes"
+else
+	echo "No oh-my-zsh or .dotfiles found in home directory"
+fi
