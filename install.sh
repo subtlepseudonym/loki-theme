e51478cdcca4ad277de2585ec204146c72f87802
Connor Demille <subtlepseudonym@gmail.com>
2023-02-13 12:00:35 -0500

    Specify git-patch install

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if [ -d "${HOME}/.oh-my-zsh" ]; then
 	cp `find "${dir}" -name "*\.zsh"` "${HOME}/.oh-my-zsh/custom/"
 	cp `find "${dir}" -name "*\.zsh-theme"` "${HOME}/.oh-my-zsh/custom/themes"
 elif [ ! -z "${DOTFILES}" -a -d "${DOTFILES}/themes" ]; then
-	cp `find "${dir}" -name "*\.zsh"` "${ZSH}"
+	cp `find "${dir}" -name "git-patch.zsh"` "${DOTFILES}/git"
 	cp `find "${dir}" -name "*\.zsh-theme"` "${DOTFILES}/themes"
 else
 	echo "No oh-my-zsh or .dotfiles/themes found in home directory"
